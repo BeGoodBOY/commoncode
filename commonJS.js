72a02a028cc24cf2850de79b348ec6f4dd1e6d33
ligaoshang <lbstc888@163.com>
2017-09-21 17:21:46 +0800

    comJs

--- commonJS.js
+++ commonJS.js
@@ -127,3 +127,24 @@ function backTop(btnId) {
         btn.style.display = (d.scrollTop + b.scrollTop > 100) ? 'block' : "none"
     }
 }
+
+//钱格式化
+function formatMoney(number, places, symbol, thousand, decimal) {
+    number = number || 0;
+    places = !isNaN(places = Math.abs(places)) ? places : 2;
+    symbol = symbol !== undefined ? symbol : "";
+    thousand = thousand || ",";
+    decimal = decimal || ".";
+    var negative = number < 0 ? "-" : "",
+        i = parseInt(number = Math.abs(+number || 0).toFixed(places), 10) + "",
+        j = (j = i.length) > 3 ? j % 3 : 0;
+    return symbol + negative + (j ? i.substr(0, j) + thousand : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousand) + (places ? decimal + Math.abs(number - i).toFixed(places).slice(2) : "");
+},
+
+//截取字符串
+function textless(str,len) {
+    if (str.length > len) {
+        str = str.substring(0, len) + "..."
+    }
+    return str
+}
